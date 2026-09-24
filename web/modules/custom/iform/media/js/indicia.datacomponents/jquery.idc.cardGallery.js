b8431595f0d77b86330326ef32b15eaa6932cef2
Biren <bire@ceh.ac.uk>
2026-09-24 16:31:45 +0100

    update iForm to 9.27.1

--- web/modules/custom/iform/media/js/indicia.datacomponents/jquery.idc.cardGallery.js
+++ web/modules/custom/iform/media/js/indicia.datacomponents/jquery.idc.cardGallery.js
@@ -775,7 +775,7 @@
       }
       $.each($('.idc-control'), function() {
         var controlClass = $(this).data('idc-class');
-        if (typeof this.callbacks.itemUpdate !== 'undefined') {
+        if (this.callbacks && this.callbacks.itemUpdate) {
           $(this)[controlClass]('on', 'itemUpdate', (item) => {
             $(item).removeClass('selected');
             while (item.length > 0 && $(item).hasClass('disabled')) {
